31801e938589288071558bd0f2e4760954ccc8d0
Yohanes William <william.gunawan@live.com>
2017-11-08 09:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+  return
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  return
+}
+
