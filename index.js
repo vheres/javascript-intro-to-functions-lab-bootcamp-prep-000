05595400b94a1b9dc951cd1875da48da9044ba73
Yohanes William <william.gunawan@live.com>
2017-11-08 09:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string === '')
+  if (string === 'I love you, Grandma.')
   else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
